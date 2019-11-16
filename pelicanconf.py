c6ecb5094e55a79e268f65d4a39e859868cc7585
Pete Bunting <petebunting@mac.com>
2019-11-16 10:22:07 +0000

    Update with theme in the repo.

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,7 +6,7 @@ AUTHOR = 'Pete Bunting'
 SITENAME = 'Remote Sensing dot Info'
 SITEURL = ''
 
-THEME = '/Users/pete/Development/pelican-blueidea'
+THEME = 'theme'
 
 
 PATH = 'content'
@@ -23,18 +23,20 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('RSGISLib', 'https://www.rsgislib.org'),
+         ('ARCSI', 'https://arcsi.remotesensing.info'),
+         ('EODataDown', 'https://eodatadown.remotesensing.info'),
+         ('KEALib', 'https://kealib.org'),
+         ('SPDlib', 'https://www.spdlib.org'),
+         ('pylidar', 'https://pylidar.org'),
+         ('TuiView', 'https://tuiview.org'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Bitbucket', 'https://bitbucket.org/petebunting/'),
+         ('GitHub', 'https://github.com/petebunting'),)
 
 DEFAULT_PAGINATION = False
 
-PLUGINS = ['pelican_youtube']
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
